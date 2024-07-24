a9252b408bc934a26cb3c434c41688564dc4e2e7
KeyseyDee <stephkeysey030@gmail.com>
2024-07-24 13:26:11 +0000

    fire

---
+++ src/images/index.ts
@@ -0,0 +1,27 @@
+import hamsterExchange from "./Husky2.png";
+import binanceLogo from "./Husky2.png";
+import dollarCoin from "./dollar-coin.png";
+import dailyReward from "./daily-reward.png";
+import dailyCipher from "./daily-cipher.png";
+import dailyCombo from "./daily-combo.png";
+import mainCharacter from "./Husky.png";
+import hamsterCoin from "./download-removebg-preview.png";
+import study from "./Study.png"
+import friends from "./Friendz.png"
+import task from "./Task.png"
+
+
+
+export {
+    hamsterExchange,
+    binanceLogo,
+    dollarCoin,
+    dailyReward,
+    dailyCipher,
+    dailyCombo,
+    mainCharacter,
+    hamsterCoin,
+    study,
+    friends,
+    task
+};
